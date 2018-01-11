d81335accb4c244cfe7daa43f28aed4c94b03235
Aaron Vinson <avinson.public@gmail.com>
2018-01-10 20:19:24 -0800

    label package as edge and main for dependent edge builds

--- conda/upload.sh
+++ conda/upload.sh
@@ -2,5 +2,5 @@
 set -ev
 
 conda install anaconda-client
-anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/libprotobuf-*.tar.bz2 --force
-anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/distributions-*.tar.bz2 --force
+anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l main -l edge ~/miniconda/conda-bld/linux-64/libprotobuf-*.tar.bz2 --force
+anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l main -l edge ~/miniconda/conda-bld/linux-64/distributions-*.tar.bz2 --force
